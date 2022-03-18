497e5d01d518ebdbe6b53c09461c3769d6ad199f
Yuepeng <y.yang@cfa-insta.fr>
2022-03-18 09:36:48 +0100

    ok

--- Youtube_API.py
+++ Youtube_API.py
@@ -1,5 +1,7 @@
+from textwrap import indent
 from urllib import request, response
 from googleapiclient.discovery import build
+import json
 
 
 api_key = 'AIzaSyBlFVhDLPeriijIn2pgCKJmRkC3jpS_inQ'
@@ -7,11 +9,18 @@ api_key = 'AIzaSyBlFVhDLPeriijIn2pgCKJmRkC3jpS_inQ'
 youtube = build('youtube', 'v3', developerKey=api_key)
 # ...
 
+titre = '中国话'
+
 request = youtube.search().list(
     part='snippet',
-    q='baby justin',
+    q=titre,
 )
-
 response = request.execute()
 
-print(response)
+    
+#fichier = open("result.txt", "w")
+#json.dump(response, fichier, indent = 6)
+#fichier.close()
+
+print(response['items'][0]['snippet']['title'])
+print('https://www.youtube.com/watch?v='+response['items'][0]['id']['videoId'])
